593b70e9f8945c7cf16a2b7abce4a48072ffdb49
firasjaber <firasjaberc@gmail.com>
2021-01-04 21:54:42 +0100

    finished book card

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,18 +1,98 @@
 import React from 'react';
-import { Box, Text, Heading, Grid, SimpleGrid } from '@chakra-ui/react';
+import {
+  Box,
+  Image,
+  Heading,
+  Text,
+  SimpleGrid,
+  useColorModeValue,
+  IconButton,
+} from '@chakra-ui/react';
+import { FiLogOut } from 'react-icons/fi';
 
 const Home = () => {
+  const bg = useColorModeValue('gray.100', 'gray.700');
   return (
     <Box px="4" py="2">
       <Heading as="h2" size="lg" mb="4">
         Your Books
       </Heading>
       <SimpleGrid minChildWidth="170px" spacing="20px">
-        <Box w="100%" h="400px" bg="blue.500" />
-        <Box w="100%" h="400px" bg="blue.500" />
-        <Box w="100%" h="400px" bg="blue.500" />
-        <Box w="100%" h="400px" bg="blue.500" />
-        <Box w="100%" h="400px" bg="blue.500" />
+        <Box w="100%" h="395px" bg={bg} rounded="md" boxShadow="base">
+          <Image
+            roundedTop="md"
+            boxSize="300px"
+            objectFit="cover"
+            src="https://books.google.com/books/content?id=trakSLk9qt0C&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+            alt="Gay Science"
+          />
+          <Box padding="3" marginTop="0.5">
+            <Heading as="h4" size="md">
+              The Gay Science
+            </Heading>
+            <Text size="sm">Friedrich Nietzshe</Text>
+          </Box>
+        </Box>
+        <Box w="100%" h="395px" bg={bg} rounded="md" boxShadow="base">
+          <Image
+            roundedTop="md"
+            boxSize="300px"
+            objectFit="cover"
+            src="https://books.google.com/books/content?id=trakSLk9qt0C&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+            alt="Gay Science"
+          />
+          <Box padding="3" marginTop="0.5">
+            <Heading as="h4" size="md">
+              The Gay Science
+            </Heading>
+            <Text size="sm">Friedrich Nietzshe</Text>
+          </Box>
+        </Box>{' '}
+        <Box w="100%" h="395px" bg={bg} rounded="md" boxShadow="base">
+          <Image
+            roundedTop="md"
+            boxSize="300px"
+            objectFit="cover"
+            src="https://books.google.com/books/content?id=trakSLk9qt0C&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+            alt="Gay Science"
+          />
+          <Box padding="3" marginTop="0.5">
+            <Heading as="h4" size="md">
+              The Gay Science
+            </Heading>
+            <Text size="sm">Friedrich Nietzshe</Text>
+          </Box>
+        </Box>{' '}
+        <Box w="100%" h="395px" bg={bg} rounded="md" boxShadow="base">
+          <Image
+            roundedTop="md"
+            boxSize="300px"
+            objectFit="cover"
+            src="https://books.google.com/books/content?id=trakSLk9qt0C&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+            alt="Gay Science"
+          />
+          <Box padding="3" marginTop="0.5">
+            <Heading as="h4" size="md">
+              The Gay Science
+            </Heading>
+            <Text size="sm">Friedrich Nietzshe</Text>
+          </Box>
+        </Box>{' '}
+        <Box w="100%" h="395px" bg={bg} rounded="md" boxShadow="base">
+          <Image
+            roundedTop="md"
+            boxSize="300px"
+            objectFit="cover"
+            src="https://books.google.com/books/content?id=trakSLk9qt0C&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+            alt="Gay Science"
+          />
+          <Box padding="3" marginTop="0.5">
+            <Heading as="h4" size="md">
+              The Gay Science
+            </Heading>
+            <Text size="sm">Friedrich Nietzshe</Text>
+          </Box>
+        </Box>
       </SimpleGrid>
     </Box>
   );
