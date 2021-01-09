82fad67a6035a52fd94fdda2f020a5792e9497b4
Firas Jaber <firasjaberc@gmail.com>
2021-01-09 11:56:03 +0100

    login & signup page components design

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,8 @@ import { ChakraProvider, theme, Container } from '@chakra-ui/react';
 import Header from './ui/Header';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 import SearchPage from './pages/SearchPage';
 
 function App() {
@@ -10,12 +12,19 @@ function App() {
     <ChakraProvider theme={theme}>
       <Router>
         <Container maxW="3xl" py="2">
-          <Header />
           <Switch>
             <Route path="/search">
+              <Header />
               <SearchPage />
             </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/signup">
+              <SignupPage />
+            </Route>
             <Route>
+              <Header />
               <HomePage path="/" />
             </Route>
           </Switch>
